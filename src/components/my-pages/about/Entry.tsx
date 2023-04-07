060d5c21f71e583d0f82d8a1a80ac398806eef4a
ruben00001 <a.ruben00001@gmail.com>
2023-04-07 13:56:50 +0100

    .

--- src/components/my-pages/about/Entry.tsx
+++ src/components/my-pages/about/Entry.tsx
@@ -24,7 +24,7 @@ export default AlbumsPage;
 const Layout = ({ children }: { children: ReactElement | ReactElement[] }) => {
   return (
     <div className="min-h-screen overflow-x-hidden">
-      <Header color="white" hideLogoAndUserMenuWhenClosed />
+      <Header color="white" hideLogo hideUserMenu />
       {children}
     </div>
   );
@@ -46,9 +46,9 @@ const BannerImage = () => {
 
 const SocialLinks = () => {
   return (
-    <div className="mt-2 flex flex-col gap-2 px-4 pt-1 text-sm sm:mt-0 sm:flex-row sm:gap-8 md:gap-12 md:text-base">
-      <div className="flex gap-2 md:gap-4">
-        <p className="text-gray-400">contact</p>
+    <div className="mt-2 flex flex-col gap-2 px-4 pt-1 text-sm font-light sm:mt-0 sm:flex-row sm:gap-8 md:gap-12 md:text-base">
+      <div className="flex items-center gap-2 md:gap-4">
+        <p className=" text-gray-400">contact</p>
         <a href="mailto:pirospixs@gmail.com" target="_blank">
           pirospixs@gmail.com
         </a>
@@ -80,7 +80,9 @@ const MainText = () => {
 
   return (
     <div className="mt-12 flex justify-center px-4 md:mt-16">
-      <p className="whitespace-pre-line font-serif">{textData.body}</p>
+      <p className="whitespace-pre-line font-serif-3 text-xl">
+        {textData.body}
+      </p>
     </div>
   );
 };
